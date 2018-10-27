b038c8dd3e98c8b9e4116f02deb18b68741bcf54
Lev Stolpner <l.a.stolpner@gmail.com>
2018-10-27 18:17:37 +0300

    enemy board initialization

--- src/ru/stolpner/battleship/board/EnemyBoard.java
+++ src/ru/stolpner/battleship/board/EnemyBoard.java
@@ -2,6 +2,21 @@ package ru.stolpner.battleship.board;
 
 import ru.stolpner.battleship.cell.EnemyBoardCell;
 
+import static ru.stolpner.battleship.util.Constants.BOARD_SIZE;
+
 public class EnemyBoard {
-    EnemyBoardCell[] cells;
+   private EnemyBoardCell[][] cells;
+
+    public EnemyBoard() {
+        initialize();
+    }
+
+    private void initialize() {
+        cells = new EnemyBoardCell[BOARD_SIZE][BOARD_SIZE];
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                cells[i][j] = new EnemyBoardCell(i, j);
+            }
+        }
+    }
 }
--- src/ru/stolpner/battleship/cell/EnemyBoardCell.java
+++ src/ru/stolpner/battleship/cell/EnemyBoardCell.java
@@ -1,4 +1,13 @@
 package ru.stolpner.battleship.cell;
 
 public class EnemyBoardCell {
+    private int x;
+    private int y;
+    private boolean isShot;
+    private boolean isHit;
+
+    public EnemyBoardCell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
