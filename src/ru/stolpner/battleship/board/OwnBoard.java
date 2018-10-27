4dfd6704522750cd495ccf59e927b35be19d849e
Lev Stolpner <l.a.stolpner@gmail.com>
2018-10-27 17:07:36 +0300

    ships placing on own board

--- src/ru/stolpner/battleship/board/OwnBoard.java
+++ src/ru/stolpner/battleship/board/OwnBoard.java
@@ -11,8 +11,8 @@ import static ru.stolpner.battleship.util.Constants.BOARD_SIZE;
 import static ru.stolpner.battleship.util.Constants.MAX_NUMBER_OF_SHIPS;
 
 public class OwnBoard {
-    OwnBoardCell[][] cells;
-    Ship[] ships;
+    private OwnBoardCell[][] cells;
+    private Ship[] ships;
 
     public OwnBoard() {
         initialize();
@@ -24,7 +24,19 @@ public class OwnBoard {
     }
 
     public void placeShip(int x, int y, int length, Rotation rotation) {
+        Ship ship = new Ship(x, y, length, rotation);
+        addShip(ship);
+        setShipToCells(ship);
+    }
 
+    private void initialize() {
+        ships = new Ship[MAX_NUMBER_OF_SHIPS];
+        cells = new OwnBoardCell[BOARD_SIZE][BOARD_SIZE];
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                cells[i][j] = new OwnBoardCell();
+            }
+        }
     }
 
     private boolean isPlacementInBounds(int x, int y, int length, Rotation rotation) {
@@ -43,10 +55,10 @@ public class OwnBoard {
     }
 
     private boolean isPlaceOccupiedByShip(Ship ship, int x, int y, int length, Rotation rotation) {
-        int shipLeftUpperCornerX = ship.getPositionX() - 1;
-        int shipLeftUpperCornerY = ship.getPositionY() - 1;
-        int shipRightBottomCornerX = ship.getPositionX() + (ship.isVertical() ? 1 : ship.getLength());
-        int shipRightBottomCornerY = ship.getPositionY() + (ship.isVertical() ? ship.getLength() : 1);
+        int shipLeftUpperCornerX = ship.getX() - 1;
+        int shipLeftUpperCornerY = ship.getY() - 1;
+        int shipRightBottomCornerX = ship.getX() + (ship.isVertical() ? 1 : ship.getLength());
+        int shipRightBottomCornerY = ship.getY() + (ship.isVertical() ? ship.getLength() : 1);
 
         int newShipLeftUpperCornerX = x - 1;
         int newShipLeftUpperCornerY = y - 1;
@@ -57,13 +69,26 @@ public class OwnBoard {
                 shipRightBottomCornerX > newShipLeftUpperCornerX && shipRightBottomCornerY > newShipLeftUpperCornerY;
     }
 
-    private void initialize() {
-        ships = new Ship[MAX_NUMBER_OF_SHIPS];
-        cells = new OwnBoardCell[BOARD_SIZE][BOARD_SIZE];
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                cells[i][j] = new OwnBoardCell();
+    private void addShip(Ship ship) {
+        for (int i = 0; i < ships.length; i++) {
+            if (ships[i] == null) {
+                ships[i] = ship;
+                break;
             }
+        }
+    }
+
+    private void setShipToCells(Ship ship) {
+        switch (ship.getRotation()) {
+            case VERTICAL:
+                for (int i = ship.getY(); i < ship.getY() + ship.getLength(); i++) {
+                    cells[ship.getX()][i].setShip(ship);
+                }
+                break;
+            case HORIZONTAL:
+                for (int i = ship.getX(); i < ship.getX() + ship.getLength(); i++) {
+                    cells[i][ship.getY()].setShip(ship);
+                }
         }
     }
 }
--- src/ru/stolpner/battleship/cell/OwnBoardCell.java
+++ src/ru/stolpner/battleship/cell/OwnBoardCell.java
@@ -1,4 +1,9 @@
 package ru.stolpner.battleship.cell;
 
+import ru.stolpner.battleship.Ship;
+
 public class OwnBoardCell {
+    public void setShip(Ship ship) {
+
+    }
 }
