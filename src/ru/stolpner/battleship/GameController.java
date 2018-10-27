06091989e36cf5e31887a83c060eb042698e7f0b
Lev Stolpner <l.a.stolpner@gmail.com>
2018-10-27 18:30:05 +0300

    stage separation

--- src/ru/stolpner/battleship/GameController.java
+++ src/ru/stolpner/battleship/GameController.java
@@ -2,6 +2,7 @@ package ru.stolpner.battleship;
 
 import ru.stolpner.battleship.player.Bot;
 import ru.stolpner.battleship.player.Human;
+import ru.stolpner.battleship.util.Printer;
 
 public class GameController {
     private Human human;
@@ -13,7 +14,20 @@ public class GameController {
     }
 
     public void startGame() {
+        prepareShips();
+        startShooting();
+    }
+
+    private void prepareShips() {
+        Printer.printLine("Starting the game...");
         bot.placeShips();
+        Printer.printLine("I've placed my ships, your turn.");
         human.placeShips();
+        Printer.printLine("OK, we are ready.");
+    }
+
+    private void startShooting() {
+        Printer.printLine("Time to shoot! You first.");
+        //TODO add shooting stage logic here
     }
 }
